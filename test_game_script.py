91c4d45e2c5eab77b779644fdf2f4f0bd9850f0d
Abiodun Ajibike <abiodun.ajibike@buto.tv>
2018-10-25 23:15:03 +0100

    add test for deal_card

--- test_game_script.py
+++ test_game_script.py
@@ -11,6 +11,18 @@ class TestBlackJackGame(unittest.TestCase):
         selected_card = self.game.select_card()
         self.assertIn(selected_card, self.game.deck)
 
+    def test_deal_card(self):
+        hand = []
+        # deal 1 card
+        self.game.deal_card(hand, 1)
+        self.assertEqual(len(hand), 1)
+        self.assertEqual(len(self.game.deck), 51)
+        # clear hand and deal 2 cards
+        hand.clear()
+        self.game.deal_card(hand, 2)
+        self.assertEqual(len(hand), 2)
+        self.assertEqual(len(self.game.deck), 49)
+
 
 if __name__ == '__main__':
     unittest.main()
